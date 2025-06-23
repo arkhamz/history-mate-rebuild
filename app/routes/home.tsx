a99145a61cf0086a717f4a14fe52964d2aedda3a
arkhamz <68877605+arkhamz@users.noreply.github.com>
2025-06-23 17:58:40 +0200

    remove-netlify-plugin-and-fix-name-casing

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router";
-import SpringFade from "../components/spring-fade/SpringFade";
+import SpringFade from "~/components/spring-fade/SpringFade";
 import "./Home.css";
 
 function Home() {
