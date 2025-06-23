f9c003448953c6a7c2deffe33597b36ce6db1ba3
arkhamz <68877605+arkhamz@users.noreply.github.com>
2025-06-23 23:05:46 +0200

    remove-spinner-fragments

--- app/components/spinner/Spinner.tsx
+++ app/components/spinner/Spinner.tsx
@@ -3,21 +3,17 @@ import { GiCrossedSwords } from "react-icons/gi";
 
 function Spinner() {
   return (
-    <>
-      (
-      <div className="spinner-container">
-        <span className="sword sword-1">
-          <GiCrossedSwords />
-        </span>
-        <span className="sword sword-2">
-          <GiCrossedSwords />
-        </span>
-        <span className="sword sword-3">
-          <GiCrossedSwords />
-        </span>
-      </div>
-      )
-    </>
+    <div className="spinner-container">
+      <span className="sword sword-1">
+        <GiCrossedSwords />
+      </span>
+      <span className="sword sword-2">
+        <GiCrossedSwords />
+      </span>
+      <span className="sword sword-3">
+        <GiCrossedSwords />
+      </span>
+    </div>
   );
 }
 
